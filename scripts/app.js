ab2dd855e7f3e5c51dda09524376351748cbde13
Dreaker <dreakerguides@gmail.com>
2025-03-02 14:50:00 +0100

    Added skeleton for Digimon information and added initial information for most Digimon (A lot information is still missing). Show the information of every Digimon line and the image of every Digimon as a placeholder

---
+++ scripts/app.js
@@ -0,0 +1,125 @@
+// TODO: Add an option to choose between Japanese, English or In-Game Digimon names
+
+// TODO: Use the following property for graying out attributes that have not been maxed:
+// filer: grayscale(100%)
+// NOTE: Might also need to add something else to show when the attribute has been maxed, to help with color blindness
+
+// DEBUG: Function to show the portraits of all currently added Digimon to spot any errors. Remove when releasing
+function DEBUG_DISPLAY() {
+    // Displays the info cards for every Digimon (Will later become the Digimon menu)
+    let baseDigimonArray = [];
+    Object.values(DIGIMON).forEach(digimonId => {
+        Object.values(DIGIMON_DATA[digimonId].base).forEach(baseDigimon => {
+            if (baseDigimonArray.indexOf(baseDigimon) == -1) {
+                displayPreviewDigimon(baseDigimon);
+                baseDigimonArray.push(baseDigimon);
+            }
+        });
+    });
+
+    // Displays the icons for every Digimon
+    let image;
+    Object.values(DIGIMON).forEach(digimonData => {
+        image = document.createElement("img");
+        image.src = "images/icons/digimon/" + DIGIMON_DATA[digimonData].id + ".png";
+        image.alt = DIGIMON_DATA[digimonData].id;
+        document.getElementsByTagName("body")[0].appendChild(image);
+    });
+}
+
+// TODO: Function to display the Digimon passed in in the preview
+function displayPreviewDigimon(digimonID) {
+    let template = document.getElementById("digimon-preview");
+    let digimonPreviewDiv = document.getElementById("digimon-list");
+    // html div holding all the Digimon's information
+    let digimonInfo;
+    // Array holding all the different evolutions of this Digimon
+    let evolutions = [];
+    // Array to hold all the attributes the Digimon has
+    let attributes = [];
+    // Temp array to check the attributes of all evolutions
+    let digimonLeftToCheck = [];
+    // Creates a new image for the Digimon's Attribute
+    let attributeImage = document.createElement("img");
+    // Set the data attribute of the element to 'data-icon="attribute"'
+    attributeImage.dataset.icon = "attribute";
+    attributeImage.src = "images/icons/attributes/"
+
+    digimonLeftToCheck.push(digimonID);
+    evolutions.push(digimonID);
+    rides = 0;
+
+    while (digimonLeftToCheck.length > 0) {
+        // Grab the last Digimon in the array to check its attributes
+        let digimonToCheck = digimonLeftToCheck.pop();
+
+        console.log(digimonToCheck);
+        // Save its attributes if they're not already in the array
+        if (!attributes.includes(DIGIMON_DATA[digimonToCheck].type)) {
+            attributes.push(DIGIMON_DATA[digimonToCheck].type);
+        }
+        if (!attributes.includes(DIGIMON_DATA[digimonToCheck].element)) {
+            attributes.push(DIGIMON_DATA[digimonToCheck].element);
+        }
+
+        // Add whether the evolution has a ride mode
+        rides += DIGIMON_DATA[digimonToCheck].ride_item.itemRequired == RIDE_ITEMS.NONE ? 0 : 1;
+
+        // Add this Digimon's evolutions to check their attributes as well
+        DIGIMON_EVOLUTIONS[digimonToCheck].forEach(evolution => {
+        // If the evolution evolves from the Digimon being displayed && it was not already checked
+            if (evolution.baseDigimon == digimonID && !evolutions.includes(evolution.evolvesTo)) {
+                digimonLeftToCheck.push(evolution.evolvesTo);
+                evolutions.push(evolution.evolvesTo);
+            }
+        });
+    }
+
+    digimonInfo = template.content.cloneNode(true);
+    // TODO: Change names to use the one selected by the player, instead of specifically "japanese"
+    digimonInfo.querySelector("h3").innerHTML = DIGIMON_DATA[digimonID].names.japanese;
+    digimonInfo.querySelector('img[data-icon=digimon]').src += DIGIMON_DATA[digimonID].id + ".png";
+    digimonInfo.querySelector('img[data-icon=digimon]').alt = DIGIMON_DATA[digimonID].names.japanese + " portrait";
+    
+    // Display all the evolution's attributes
+    let newAttribute;
+    attributes.forEach(attribute => {
+        newAttribute = attributeImage.cloneNode(true);
+        newAttribute.src += attribute + ".png";
+        newAttribute.alt = attribute + " Icon";
+        digimonInfo.querySelector('p[data-list=attributes]').appendChild(newAttribute);
+    });
+
+    digimonInfo.querySelector('span[data-evolutions=current]').innerHTML = 1;
+    digimonInfo.querySelector('span[data-evolutions=total]').innerHTML = evolutions.length;
+    digimonInfo.querySelector('span[data-skill-mastery=current]').innerHTML = 0;
+    digimonInfo.querySelector('span[data-skill-mastery=total]').innerHTML = evolutions.length;
+    // TODO: Uptade once Riding Modes are taken into account
+    digimonInfo.querySelector('span[data-rides=current]').innerHTML = 0;
+    digimonInfo.querySelector('span[data-rides=total]').innerHTML = rides;
+
+    digimonPreviewDiv.appendChild(digimonInfo);
+}
+
+function getLine(digimon) {
+    return DIGIMON_DATA[digimon].base[Math.floor(Math.random() * DIGIMON_DATA[digimon].base.length)];
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    // let randomDigimonArray = [];
+    // let randomDigimon;
+    // for (let index = 0; index < 3; index++) {
+    //     do {
+    //         digimon = Object.values(DIGIMON)[Math.floor(Math.random() * Object.values(DIGIMON).length)];
+    //         console.log(digimon);
+    //         randomDigimon = getLine(digimon);
+    //     } while (randomDigimonArray.indexOf(randomDigimon) != -1);
+    //     console.log(randomDigimon);
+    //     randomDigimonArray.push(randomDigimon);
+    // }
+    // displayPreviewDigimon(randomDigimonArray[0]);
+    // displayPreviewDigimon(randomDigimonArray[1]);
+    // displayPreviewDigimon(randomDigimonArray[2]);
+
+    DEBUG_DISPLAY();
+})
